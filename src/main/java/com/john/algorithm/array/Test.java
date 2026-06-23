a3cc134e42a36010d8ba570a7d2d153374437546
hanyulei <han.yulei@xyb2b.com>
2026-06-23 09:50:55 +0800

    init

---
+++ src/main/java/com/john/algorithm/array/Test.java
@@ -0,0 +1,11 @@
+package com.john.algorithm.array;
+
+/**
+ * @author : hanyulei
+ * @date : 2026-06-04 11:11:49
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(15&12);
+    }
+}
